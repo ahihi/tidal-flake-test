b921cb893cfb962ed55f82f87ecba2bf0cae76a9
miranda <miranda@pulusound.fi>
2025-02-04 10:42:43 +0200

    add overlay and package

--- flake.nix
+++ flake.nix
@@ -9,53 +9,72 @@
 
     tidal.url = "github:tidalcycles/Tidal";
     tidal.inputs.nixpkgs.follows = "nixpkgs";
+
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, darwin, tidal }: let
-    tidalModule = system: { config, pkgs, ... }:
+  outputs = { self, nixpkgs, darwin, tidal, flake-utils }:
+    let
+      tidalOverlay = final: prev:
+        let
+          system = prev.stdenv.hostPlatform.system;
+        in
+          {
+            tidal-ghc = final.haskellPackages.ghcWithPackages
+              (p: [
+                tidal.packages.${system}.tidal
+              ]);
+          };
+      tidalModule = system: { config, pkgs, ... }:
+        {
+          nixpkgs.overlays = [ tidalOverlay ];
+          environment.systemPackages = [ pkgs.tidal-ghc ];
+        };
+    in
       {
-        nixpkgs.overlays =
-          [
-            (final: prev: {
-              tidal-ghc = final.haskellPackages.ghcWithPackages
-                (p: [
-                  tidal.packages.${system}.tidal
-                ]);
-            })
-          ];
+        darwinConfigurations = {
+          "darwinExample" = darwin.lib.darwinSystem rec {
+            system = "x86_64-darwin";
+            modules = [
+              (tidalModule system)
+              # make darwin-rebuild happy
+              {
+                system.stateVersion = 5;
+              }
+            ];
+          };
+        };
+        nixosConfigurations = {
+          "nixosExample" = nixpkgs.lib.nixosSystem rec {
+            system = "x86_64-linux";
+            modules = [
+              (tidalModule system)
+              # make nixos-rebuild happy
+              {
+                system.stateVersion = "24.11";
+                boot.loader.grub.enable = true;
+                boot.loader.grub.device = "/dev/sda";
+              }
+              ./nixosExample-hardware-configuration.nix
+            ];
+          };
+        };
+        overlays.default = tidalOverlay;
+      } //
+      (flake-utils.lib.eachDefaultSystem (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [self.overlays.default];
+          };
+        in rec {
+          packages.default = pkgs.tidal-ghc;
 
-        environment.systemPackages = [
-          pkgs.tidal-ghc
-        ];
-      };
-  in
-    {
-      darwinConfigurations = {
-        "darwinExample" = darwin.lib.darwinSystem rec {
-          system = "x86_64-darwin";
-          modules = [
-            (tidalModule system)
-            # make darwin-rebuild happy
-            {
-              system.stateVersion = 5;
-            }
-          ];
-        };
-      };
-      nixosConfigurations = {
-        "nixosExample" = nixpkgs.lib.nixosSystem rec {
-          system = "x86_64-linux";
-          modules = [
-            (tidalModule system)
-            # make nixos-rebuild happy
-            {
-              system.stateVersion = "24.11";
-              boot.loader.grub.enable = true;
-              boot.loader.grub.device = "/dev/sda";
-            }
-            ./nixosExample-hardware-configuration.nix
-          ];
-        };
-      };
-    };
+          devShell = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              packages.default
+            ];
+          };
+        }
+      ));
 }
